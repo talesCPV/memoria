c476175863b7e488897a44e2b691fd94003a49ad
talesCPV <tales@flexibus.com.br>
2024-06-05 00:50:29 -0300

    by_script

--- commit.sh
+++ commit.sh
@@ -15,8 +15,8 @@ then
 
     git commit -m "by_script"
 
-#    git branch -M main
-#    git remote add origin git@github.com:talesCPV/memoria.git
+    git branch -M main
+    git remote add origin git@github.com:talesCPV/memoria.git
     git remote set-url origin git@github.com:talesCPV/memoria.git
 
     git push -u -f origin main
